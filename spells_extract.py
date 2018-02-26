8907ea5a2007995b085dff97d5502ade3edae311
Naveen Piedy <naveenpiedy@users.noreply.github.com>
2018-02-26 12:18:56 -0700

    Regex to separate Type and Pronunciation.

--- spells_extract.py
+++ spells_extract.py
@@ -20,7 +20,16 @@ main_content = soup.find('div', class_ ='tabber')
 titles = main_content.find_all('span', class_= 'mw-headline')
 body = main_content.find_all('dl')
 for title, body in zip(titles, body):
-    print(title.text)
+    #print(title.text)
+    type = re.findall(r'^Type:.*', str(body.text))
+    pronounciation = re.findall(r'Pronunciation:.*', body.text)
+    print(pronounciation)
+#    print(h[0])
+#    if len(h)>0:
+#         print(re.sub(r'Type: \b'," ",h[0]))
+    if len(pronounciation)>0:
+        print(re.sub(r'Pronunciation: \b'," ", pronounciation[0]))
     print(body.text)
+
 # for span in main_content.find_all('span', class_= 'mw-headline'):
 #     print(span.get_text())
